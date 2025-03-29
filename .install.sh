33359f88e8c4534ac91561486cb5b0559c48166f
Gunther Klessinger <gk@Gunthers-Air.fritz.box>
2025-03-29 17:44:13 +0100

    init

--- .install.sh
+++ .install.sh
@@ -238,27 +238,27 @@ source $HOME/.zshrc
 conf config --local status.showUntrackedFiles no
 
 # Python Packages
-echo "Installing Python Packages..."
-curl https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh | sh
-source $HOME/.zshrc
-conda install -c apple tensorflow-deps
-conda install -c conda-forge pybind11
-conda install matplotlib
-conda install jupyterlab
-conda install seaborn
-conda install opencv
-conda install joblib
-conda install pytables
-pip install tensorflow-macos
-pip install tensorflow-metal
-pip install debugpy
-pip install sklearn
+# echo "Installing Python Packages..."
+# curl https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh | sh
+# source $HOME/.zshrc
+# conda install -c apple tensorflow-deps
+# conda install -c conda-forge pybind11
+# conda install matplotlib
+# conda install jupyterlab
+# conda install seaborn
+# conda install opencv
+# conda install joblib
+# conda install pytables
+# pip install tensorflow-macos
+# pip install tensorflow-metal
+# pip install debugpy
+# pip install sklearn
 
 # Start Services
 echo "Starting Services (grant permissions)..."
 #brew services start skhd
 #brew services start fyabai
-brew services restart sketchybar
+#brew services restart sketchybar
 #brew services rebrew services restart svim
 
 csrutil status
